c6f613ab7cd31f5e70bb0b84d4186d70be7c91a6
Dan-Joe Lopez <Dan-Joe.Lopez@sap.com>
2020-12-18 19:57:42 +0000

    depend on hana-studio for sap-media resource

--- metadata.rb
+++ metadata.rb
@@ -3,16 +3,15 @@ maintainer       'SAP'
 maintainer_email 'Dan-Joe.Lopez@sap.com'
 license          'Apache-2.0'
 description      'Installs/Configures Hana Client for Windows only'
-source_url       'https://github.com/sap/hana-client-chef-cookbook' if respond_to?(:source_url)
-issues_url       'https://github.com/sap/hana-client-chef-cookbook/issues' if respond_to?(:issues_url)
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '3.0.1'
 
-chef_version     '>= 12' if respond_to?(:chef_version)
+chef_version     '>= 15'
 
-issues_url       'https://github.com/SAP/hana-client-chef-cookbook/issues' if respond_to?(:issues_url)
-source_url       'https://github.com/SAP/hana-client-chef-cookbook' if respond_to?(:source_url)
+issues_url       'https://github.com/SAP/hana-client-chef-cookbook/issues'
+source_url       'https://github.com/SAP/hana-client-chef-cookbook'
 
 %w( centos redhat suse opensuse windows ).each do |os|
   supports os
 end
+
+depends 'hana-studio', '~>2.0'
